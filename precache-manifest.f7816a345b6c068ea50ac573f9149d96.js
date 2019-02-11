821c7fb9818abe6c256c09950e220ec691045a6b
alexandr.kolodko <cm-brain@yandex.ru>
2019-02-12 00:13:56 +0300

    Add build

---
+++ precache-manifest.f7816a345b6c068ea50ac573f9149d96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2cbd0084357903331b91",
+    "url": "/static/js/main.2cbd0084.chunk.js"
+  },
+  {
+    "revision": "d7a596a9772fb59313f1",
+    "url": "/static/js/1.d7a596a9.chunk.js"
+  },
+  {
+    "revision": "2cbd0084357903331b91",
+    "url": "/static/css/main.a7261053.chunk.css"
+  },
+  {
+    "revision": "6b3a85a9fa36955e74c711b4881f37ab",
+    "url": "/index.html"
+  }
+];
